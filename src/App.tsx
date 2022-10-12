1b7f532e2815a1d76b6e0f0ee172964a5dccb565
Maurice <maurice@app-bar.com>
2022-10-12 11:44:05 -0400

    feat: Initial commit - remove Redux store + packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import { Provider } from 'react-redux';
 import Config from './Config';
 import Router from './router';
-import { store } from './store';
 
 const queryClient = new QueryClient();
 
 const App = (): JSX.Element => (
-  <Provider store={store}>
-    <QueryClientProvider client={queryClient}>
-      <Router />
-      {Config.nodeEnv === 'development' ? <ReactQueryDevtools initialIsOpen={false} /> : ''}
-    </QueryClientProvider>
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <Router />
+    {Config.nodeEnv === 'development' ? <ReactQueryDevtools initialIsOpen={false} /> : ''}
+  </QueryClientProvider>
 );
 
 export default App;
